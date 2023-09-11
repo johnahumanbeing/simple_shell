71a57d154b7243579cf2d3c582f9dab8fadf6a9c
johnahumanbeing <mseewak@gmail.com>
2023-09-12 00:37:00 +0300

    UNIX line interpr

--- UNIX.c
+++ UNIX.c
@@ -5,6 +5,13 @@
 
 #define BUFFER_SIZE 1024
 
+void exec_command(char *command);
+void handle_input(char *buffer);
+
+/**
+ * main - the entry point
+ * Return: zero
+ */
 int main(void)
 {
 	char *buffer;
@@ -32,29 +39,46 @@ int main(void)
 		}
 
 		buffer[chars - 1] = '\0';
-
-		if (access(buffer, F_OK) == 0)
-		{
-			pid_t pid = fork();
-
-			if (pid == 0)
-			{
-				char *args[] = {buffer, NULL};
-
-				execve(args[0], args, NULL);
-			}
-			else
-			{
-				wait(NULL);
-			}
-		}
-		else
-		{
-			printf(":
-					command not found\n", buffer);
-		}
+		handle_input(buffer);
 	}
 
 	free(buffer);
 	return (0);
+}
+
+/**
+ * exec_command - function that executes the provided command
+ * @command: the command
+ */
+void exec_command(char *command)
+{
+	if (access(buffer, F_OK) == 0)
+	{
+		pid_t pid = fork();
+
+		if (pid == 0)
+		{
+			char *args[] = {buffer, NULL};
+
+			execve(args[0], args, NULL);
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+	else
+	{
+		printf("%s: command not found\n", buffer);
+	}
+}
+
+/**
+ * handle_input - function that handles user input
+ * and calls the exec_command to execute
+ * @buffer: the buffer
+ */
+void handle_input(char *buffer)
+{
+	exec_command(buffer);
 }
