c4765c19392683569772c53a16ca59431b160f26
johnahumanbeing <mseewak@gmail.com>
2023-09-14 01:06:54 +0300

    why me

--- exit.c
+++ exit.c
@@ -14,23 +14,23 @@
  */
 char *get_path(char *cmd)
 {
-	char *path = getenv("PATH");
-	char *dir = strtok(path, ":");
-	char *full_path = malloc(MAX_PATH);
+    char *path = getenv("PATH");
+    char *dir = strtok(path, ":");
+    char *full_path = malloc(MAX_PATH);
 
-	while (dir != NULL)
-	{
-		snprintf(full_path, MAX_PATH, "%s/%s", dir, cmd);
+    while (dir != NULL)
+    {
+        snprintf(full_path, MAX_PATH, "%s/%s", dir, cmd);
 
-		if (access(full_path, X_OK) == 0)
-		{
-			return (full_path);
-		}
+        if (access(full_path, X_OK) == 0)
+        {
+            return (full_path);
+        }
 
-		dir = strtok(NULL, ":");
-	}
-	free(full_path);
-	return (NULL);
+        dir = strtok(NULL, ":");
+    }
+    free(full_path);
+    return (NULL);
 }
 
 /**
@@ -41,25 +41,25 @@ char *get_path(char *cmd)
  */
 int exec_command(char *cmd_path, char *args[])
 {
-	int status;
-	pid_t pid = fork();
+    int status;
+    pid_t pid = fork();
 
-	if (pid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-		execv(cmd_path, args);
-		perror("execv");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		wait(&status);
-	}
-	return (0);
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        execv(cmd_path, args);
+        perror("execv");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        wait(&status);
+    }
+    return (0);
 }
 
 /**
@@ -71,19 +71,20 @@ int exec_command(char *cmd_path, char *args[])
  */
 int parse_line(char *line, char *args[])
 {
-	int num_args = 0;
-	char *arg;
+    int num_args;
+    char *arg;
 
-	arg = strtok(line, " \n");
-	while (arg != NULL && num_args < MAX_ARGS - 1)
-	{
-		args[num_args++] = arg;
-		arg = strtok(NULL, " \n");
-	}
+    num_args = 0;
+    arg = strtok(line, " \n");
+    while (arg != NULL && num_args < MAX_ARGS - 1)
+    {
+        args[num_args++] = arg;
+        arg = strtok(NULL, " \n");
+    }
 
-	args[num_args] = NULL;
+    args[num_args] = NULL;
 
-	return (num_args);
+    return (num_args);
 }
 
 /**
@@ -92,40 +93,40 @@ int parse_line(char *line, char *args[])
  */
 int main(void)
 {
-	char *line = NULL;
-	size_t line_size = 0;
-	char *args[MAX_ARGS];
+    char *line = NULL;
+    size_t line_size = 0;
+    char *args[MAX_ARGS];
+    char *cmd;
 
-	while (1)
-	{
-		printf("$ ");
-		fflush(stdout);
+    while (1)
+    {
+        printf("$ ");
+        fflush(stdout);
 
-		if (getline(&line, &line_size, stdin) == -1)
-		{
-			break;
-		}
+        if (getline(&line, &line_size, stdin) == -1)
+        {
+            break;
+        }
 
-		int num_args = parse_line(line, args);
-		int num_args;
+        int num_args = parse_line(line, args);
 
-		if (num_args == 0)
-		{
-			continue;
-		}
+        if (num_args == 0)
+        {
+            continue;
+        }
 
-		char *cmd = args[0];
-		char *cmd_path = get_path(cmd);
+        cmd = args[0];
+        char *cmd_path = get_path(cmd);
 
-		if (cmd_path == NULL)
-		{
-			printf("%s: command not found\n", cmd);
-			continue;
-		}
+        if (cmd_path == NULL)
+        {
+            printf("%s: command not found\n", cmd);
+            continue;
+        }
 
-		exec_command(cmd_path, args);
-		free(cmd_path);
-	}
-	free(line);
-	return (0);
+        exec_command(cmd_path, args);
+        free(cmd_path);
+    }
+    free(line);
+    return (0);
 }
