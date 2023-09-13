52448afc5db87238c05945c2ca000dfc966fc4eb
johnahumanbeing <mseewak@gmail.com>
2023-09-13 23:05:52 +0300

    prototypes

--- shell.h
+++ shell.h
@@ -1,6 +1,10 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
+#define MAX_ARGS 64
+#define MAX_PATH 1024
+#define BUFFER_SIZE 1024
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,7 +15,21 @@
 #include <errno.h>
 
 void _printer(const char *characters);
+
 void prompt_usr(void);
+
 void execute_prompt(const char *comnd);
+
+char *get_path(char *cmd);
+
+void exec_command(char *cmd, char *cmd_path, char *args[]);
+
+void exec_command(char *command);
+
+void handle_input(char *buffer);
+
+void print_env(void);
+
+int exec_command(char *cmd_path, char *args[]);
 
 #endif
